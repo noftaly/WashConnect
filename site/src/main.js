bc3325a6446bfa0b3c791a05299fac4894179744
yohanfaj <yohan.fajerman@efrei.net>
2023-02-14 15:03:58 +0100

    bootstrap 2

--- site/src/main.js
+++ site/src/main.js
@@ -6,6 +6,10 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import './assets/main.scss'
+
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
