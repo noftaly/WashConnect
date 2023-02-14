b8715f8b4ec5281adefc3ba7e3d50fcb95860ddc
yohanfaj <yohan.fajerman@efrei.net>
2023-02-14 14:53:42 +0100

    bootstrap

--- site/src/main.js
+++ site/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
