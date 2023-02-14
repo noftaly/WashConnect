863baa340e6c1a1f043de23aae1b93a853e26d49
Elliot Maisl <elliot.maisl@gmail.com>
2023-02-14 16:15:48 +0100

    Add prisma tables

---
+++ api/prisma/migrations/20230214151035_init/migration.sql
@@ -0,0 +1,80 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `firstName` VARCHAR(255) NOT NULL,
+    `lastName` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `balance` DECIMAL(65, 2) NOT NULL DEFAULT 0,
+    `phoneNumber` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `line1` VARCHAR(255) NOT NULL,
+    `line2` VARCHAR(255) NULL,
+    `city` VARCHAR(255) NOT NULL,
+    `zip` VARCHAR(255) NOT NULL,
+    `country` ENUM('FRA', 'CHE') NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Machine` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `adTitle` VARCHAR(255) NOT NULL,
+    `adDescription` TEXT NULL,
+    `priceWashing` DECIMAL(2, 2) NULL,
+    `priceDrying` DECIMAL(2, 2) NULL,
+    `priceWashingDrying` DECIMAL(2, 2) NULL,
+    `detergentIncluded` BOOLEAN NOT NULL DEFAULT false,
+    `maxCapacity` INTEGER NOT NULL DEFAULT 0,
+    `washDuration` INTEGER NULL DEFAULT 60,
+    `dryDuration` INTEGER NULL DEFAULT 60,
+    `hasWasher` BOOLEAN NOT NULL DEFAULT true,
+    `hasDryer` BOOLEAN NOT NULL DEFAULT true,
+    `addressId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Agenda` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `timeSlot` DATETIME(3) NOT NULL,
+    `machineType` ENUM('WASHING_MACHINE', 'DRYER') NOT NULL DEFAULT 'WASHING_MACHINE',
+    `userId` INTEGER NULL,
+    `machineId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Address` ADD CONSTRAINT `Address_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Machine` ADD CONSTRAINT `Machine_addressId_fkey` FOREIGN KEY (`addressId`) REFERENCES `Address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Machine` ADD CONSTRAINT `Machine_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Agenda` ADD CONSTRAINT `Agenda_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Agenda` ADD CONSTRAINT `Agenda_machineId_fkey` FOREIGN KEY (`machineId`) REFERENCES `Machine`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
