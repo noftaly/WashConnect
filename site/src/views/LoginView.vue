f239c0ea36f7a155247a938d36810990682e5f4d
danny-grn <gr.danny75@gmail.com>
2023-03-24 12:14:00 +0100

    Login page

--- site/src/views/LoginView.vue
+++ site/src/views/LoginView.vue
@@ -5,5 +5,124 @@
 
     <br/>
     <h1 id="title" class="d-flex ml-3 justify-content-center">WashConnect</h1>
-    <h2 Class="d-flex ml-3 justify-content-center" style="font-size: 30px;">Login</h2>
+    <h2 Class="d-flex ml-3 justify-content-center" style="font-size: 30px;">Welcome back washer !</h2>
+  
+    <div class="alert alert-danger" v-if="error">
+        {{ error }}
+    </div>
+  
+        <div class="px-5 py-5">
+            <div class="form-data" v-if="!submitted">
+                
+
+
+                <!-- Username -->
+                <div class="col-md-6 forms-inputs mb-4">
+                <span>Username</span>
+                <input
+                autocomplete="off"
+                type="text"
+                v-model="username"
+                :class="{ 'form-control': true, 'is-invalid': !validName(username) && usernameBlured }"
+                @blur="usernameBlured = true"
+                @keyup.enter="submit"
+                />
+                 <div class="invalid-feedback">A valid username is required!</div>
+                </div>
+            </div>
+    
+    
+            <!-- Password -->
+            <div class="forms-inputs mb-4">
+                <span>Password</span>
+                <input
+                autocomplete="off"
+                type="password"
+                v-model="password"
+                :class="{ 'form-control': true, 'is-invalid': !validPassword(password) && passwordBlured }"
+                @blur="passwordBlured = true"
+                @keyup.enter="submit"
+                />
+                <div class="invalid-feedback">Password must have at least 8 characters!</div>
+            </div>
+            
+            <div class="mb-3">
+                <button @click.prevent="submit" class="btn btn-dark w-100">Login</button>
+            </div>
+        </div>
+
+    <p class="text-center">Don't have an account yet ?<RouterLink to="/register">Sign up here</RouterLink>.</p>
 </template>
+  
+
+<script setup>
+  import { storeToRefs } from "pinia";
+  import { ref } from "vue";
+  import router from "../router";
+  import { useAuthStore } from "../stores/auth";
+  
+  const { isAuthenticated } = storeToRefs(useAuthStore());
+  const { login } = useAuthStore();
+  
+  if (isAuthenticated.value) {
+    router.push({ name: "home" });
+  }
+  
+  const username = ref("");
+  const usernameBlured = ref(false);
+
+  const email = ref("");
+  const emailBlured = ref(false);
+  const valid = ref(false);
+  const submitted = ref(false);
+  const password = ref("");
+  const passwordBlured = ref(false);
+  
+  const error = ref("");
+  
+
+  
+  function validName(name) {
+    return name.length > 0;
+  }
+  
+  function validPassword(password) {
+    return password.length > 7;
+  }
+  
+  
+  function validate() {
+    usernameBlured.value = true;
+    passwordBlured.value = true;
+    if (
+      validName(username.value) &&
+      validPassword(password.value) 
+    )
+      valid.value = true;
+    return valid.value;
+  }
+  
+  async function submit() {
+    if (validate()) {
+      submitted.value = true;
+      try {
+        await login(
+          {
+            username: username.value,
+            password: password.value,
+          }
+        );
+        router.push({ name: "home" });
+      } catch (err) {
+        error.value = err.response?.data?.message ?? "  Oups, Something went wrong";
+      } finally {
+        submitted.value = false;
+      }
+    }
+  }
+</script>
+  
+<style scoped>
+  @import "../assets/auth.scss";
+</style>
+  
