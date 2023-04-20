87a26e1055288a50e9a9d00cc4203533c3a9748b
yohanfaj <yohan.fajerman@efrei.net>
2023-04-20 19:14:09 +0200

    corrected axios url

--- site/src/utils/axios.js
+++ site/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "http://localhost:5050",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
