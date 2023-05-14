ec2e2abd16554e2040cbc9eff195696fa68b1d48
Elliot Maisl <elliot.maisl@gmail.com>
2023-05-14 17:25:35 +0200

    make /users/ routes work

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -1,7 +1,27 @@
+import { db } from '../database.js';
+import { safeUnknownUser } from '../utils/safeUser.js';
+
 export async function findOne(req, res) {
-  res.json({ message: 'Endpoint /users/:userId' })
+  const user = await db.user.findUnique({
+    where: { id: Number(req.params.userId) },
+    include: {
+      Machine: true,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json(safeUnknownUser(user));
 }
 
 export async function findAll(req, res) {
-  res.json({ message: 'Endpoint /users' })
+  const users = await db.user.findMany({
+    include: {
+      Machine: true,
+    },
+  });
+
+  res.json(users.map(safeUnknownUser));
 }
