5020ef851e22fc4d6aad4ccbefd54ce1851ca00d
Amine2111 <94388887+Amine2111@users.noreply.github.com>
2023-05-18 20:27:45 +0200

    fusion de balance et de profile

--- site/src/views/ProfileView.vue
+++ site/src/views/ProfileView.vue
@@ -1,7 +1,28 @@
 <template>
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-sm-8 col-md-6 col-lg-4">
+        <div class="col-lg-4">
+          <!-- Balance card -->
+          <div class="card mt-5">
+            <div class="card-header bg-primary text-white text-center">
+              <h1>Your Balance</h1>
+            </div>
+            <div class="card-body">
+              <p v-if="loading" class="text-center">Loading...</p>
+              <div v-else-if="user.balance >= 0" class="d-flex justify-content-center align-items-center">
+                <h3>{{ user.balance }}€</h3>
+                <img src="../assets/piece.png" alt="piece" class="icon ml-2">
+              </div>
+              <p v-else class="text-danger text-center">Une erreur est survenue lors du chargement de votre solde.</p>
+            </div>
+            <div class="card-footer text-center">
+              <router-link to="/topup">
+                <button class="btn btn-secondary">Top Up Balance</button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8">
           <!-- User information -->
           <div class="card mt-5" v-if="user">
             <div class="card-header bg-info text-white text-center">
@@ -40,12 +61,27 @@
   </template>
   
   <script setup>
+    import { ref } from 'vue';
     import { storeToRefs } from "pinia";
     import { useAuthStore } from "../stores/auth.js";
-    
-    const { user } = storeToRefs(useAuthStore());
-  </script>
   
+    const { user, getBalance } = storeToRefs(useAuthStore());
+    const loading = ref(true);
+  
+    const fetchBalance = async () => {
+      try {
+        await getBalance();
+        loading.value = false;
+      } catch (err) {
+        console.error(err);
+        user.balance = -1;
+        loading.value = false;
+      }
+    };
+  
+    fetchBalance();
+  </script>
+   
   <style scoped>
     .icon {
       height: 20px;
