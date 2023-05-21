8e0cf18db6b679d1b896d060fd02ec471950f07f
Elliot Maisl <elliot.maisl@gmail.com>
2023-05-21 22:28:30 +0200

    Fix address validation as APIs are down

--- api/src/controllers/addresses.js
+++ api/src/controllers/addresses.js
@@ -4,25 +4,25 @@ import axios from 'axios';
 export async function getPersonalAddresses(req, res) {
   // I request the set of addresses that belong to the user
   // I perform a query to the database
-  const addresses = await db.Address.findMany({ where: { userId: req.userId}, })
+  const addresses = await db.Address.findMany({ where: { userId: req.user.id}, })
     // I send the addresses back to the client but remove the userId and createdAt fields from the response
-    res.json(addresses.map(address => ({id: address.id, 
+    res.json(addresses.map(address => ({id: address.id,
         streetL1: address.line1,
-        streetL2: address.line2, 
-        city: address.city, 
-        country: address.country, 
-        zip: address.zip, 
+        streetL2: address.line2,
+        city: address.city,
+        country: address.country,
+        zip: address.zip,
         createdAt: address.createdAt})));
 }
 
 export async function getPersonalAddressById(req, res) {
     const address = await db.Address.findUnique({ where: { id: Number(req.params.addressId) }, });
-    res.json({ 
+    res.json({
         streetL1: address.line1,
-        streetL2: address.line2, 
-        city: address.city, 
-        country: address.country, 
-        zip: address.zip, 
+        streetL2: address.line2,
+        city: address.city,
+        country: address.country,
+        zip: address.zip,
         createdAt: address.createdAt});
     }
 
@@ -31,64 +31,68 @@ export async function createPersonalAddress(req, res) {
     if (!req.body.streetL1 || !req.body.city || !req.body.country || !req.body.zip) {
         return res.status(400).json({ message: 'Missing required fields' });
     }
-    // I modify the country name to the ISO alpha-3 format
-    let country;
-    try{
-        country = await axios.get(`https://restcountries.com/v3.1/name/${req.body.country}`);
-    } catch (error) {
+    // The country API is of course down the day before the demo, so we do an ad-hoc fix
+    // let country;
+    // try{
+    //     country = await axios.get(`https://restcountries.com/v3.1/name/${req.body.country}`);
+    // } catch (error) {
+    //     return res.status(404).json({ message: 'Country not found' });
+    // }
+    if (!['france', 'suisse'].includes(req.body.country.toLowerCase())) {
         return res.status(404).json({ message: 'Country not found' });
     }
-    
-    req.body.country = country.data[0].cca3;
-    let alpha2code = country.data[0].cca2;
-    let response;
-    // I make an HTTP request to an external API
-    try{
-        response = await axios.get(`https://api.zippopotam.us/${alpha2code}/${req.body.zip}`); 
-    } catch (error) {
-        return res.status(404).json({ message: 'Zip code not found' });
-    }
+
+    const alpha2code = req.body.country.toLowerCase() === 'france' ? 'FRA' : 'CHE';
+    // let response;
+    // The zip code API is of course down the day before the demo, so we ignore ZIP code validation
+    // try{
+    //     response = await axios.get(`https://api.zippopotam.us/${alpha2code}/${req.body.zip}`);
+    // } catch (error) {
+    //     return res.status(404).json({ message: 'Zip code not found' });
+    // }
 
     // I verify that that the city and zip code match, it loops through the places in the response
-    let cityFound = false;
-    for (let i = 0; i < response.data.places.length; i++) {
-        if (response.data.places[i]['place name'].toLowerCase() === req.body.city.toLowerCase()) {
-            cityFound = true;
-            req.body.city = response.data.places[i]['place name'];
-            break;
-        }
-    }
+    // let cityFound = false;
+    // for (let i = 0; i < response.data.places.length; i++) {
+    //     if (response.data.places[i]['place name'].toLowerCase() === req.body.city.toLowerCase()) {
+    //         cityFound = true;
+    //         req.body.city = response.data.places[i]['place name'];
+    //         break;
+    //     }
+    // }
     // I anticipate the error if the city and zip code do not match
-    if (!cityFound) {
-        return res.status(400).json({ message: 'City and zip code do not match' });
-    }
+    // if (!cityFound) {
+    //     return res.status(400).json({ message: 'City and zip code do not match' });
+    // }
+
     // I create the address in the database
     const address = await db.Address.create({
         data: {
             line1: req.body.streetL1,
             line2: req.body.streetL2,
             city: req.body.city,
-            country: req.body.country,
+            country: alpha2code,
             zip: req.body.zip,
             userId: req.user.id,
         }
     });
-    // I get the address by comparing the streetL1, city, country and zip fields
-    // I get the set of address for a given userID and pick the one with the highest id
-    const addressCreated = await db.Address.findFirst({ where: { userId: req.user.id, line1: req.body.streetL1, city: req.body.city, country: req.body.country, zip: req.body.zip }, orderBy: { id: 'desc' },
-      });
     // I send the address back to the client
-    res.json({ 
-        id: addressCreated.id,
+    res.json({
+        id: address.id,
         streetL1: address.line1,
-        streetL2: address.line2, 
-        city: address.city, 
-        country: address.country, 
-        zip: address.zip, 
-        createdAt: address.createdAt});
+        streetL2: address.line2,
+        city: address.city,
+        country: address.country,
+        zip: address.zip,
+        createdAt: address.createdAt,
+    });
 }
 
-export async function removePersonalAddress(req, res){
+export async function removePersonalAddress(req, res) {
+    if (isNaN(Number(req.params.addressId))) {
+        return res.status(400).json({ message: 'Invalid address ID' });
+    }
+
     // Verify authenticated
     if (!req.user) {
         return res.status(401).json({ message: 'Unauthorized' });
